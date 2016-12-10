bfe0fcf8999ec33e77cd902a43b907630cf3a20f
Axel DEAU <dev@crossfire.fr>
2016-12-10 19:12:13 +0000

    Adding logging

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"./errors"
+	"./logger"
 )
 
 func main() {
 	fmt.Println("Hello World !")
+	l := logger.NewLogger()
+	l.SetLevel(logger.Warn)
+	l.Trace(errors.Err{"test", "message"})
+	l.Debug(errors.Err{"test", "message"})
+	l.Info(errors.Err{"test", "message"})
+	l.Warn(errors.Err{"test", "message"})
+	l.Error(errors.Err{"test", "message"})
+	l.Fatal(errors.Err{"test", "message"})
 }
